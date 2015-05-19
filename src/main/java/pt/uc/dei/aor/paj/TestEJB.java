b731f1a347e4ba95820183e1b4263afd88007098
Joao Martins <joao.pedro.brandao.martins@gmail.com>
2015-05-19 19:25:48 +0100

    Maven dependencies
    JPA
    hibernate
    postgreSQL

---
+++ src/main/java/pt/uc/dei/aor/paj/TestEJB.java
@@ -0,0 +1,18 @@
+package pt.uc.dei.aor.paj;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class TestEJB
+ */
+@Stateless
+public class TestEJB implements TestEJBRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public TestEJB() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ src/main/java/pt/uc/dei/aor/paj/TestEJBRemote.java
@@ -0,0 +1,8 @@
+package pt.uc.dei.aor.paj;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface TestEJBRemote {
+
+}
