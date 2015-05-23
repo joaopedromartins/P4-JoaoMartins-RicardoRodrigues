08f2efe8be9ff83546eec9eabeadbc9993eecb89
joaopedromartins <joao.pedro.brandao.martins@gmail.com>
2015-05-23 14:59:00 +0100

    Estrutura do monitor JOAO
    Build sem erros mas da erro wildfly associado a porta

---
+++ P4-EJB/src/main/java/pt/uc/dei/aor/paj/UserServices.java
@@ -0,0 +1,16 @@
+package pt.uc.dei.aor.paj;
+
+import javax.ejb.Stateless;
+
+/**
+ * @author jncor
+ *
+ */
+@Stateless
+public class UserServices {
+
+	public String sayHello(){
+		return "hello";
+	}
+
+}
