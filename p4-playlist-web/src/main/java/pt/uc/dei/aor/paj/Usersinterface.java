c3823edf638b4b2646d495a459a366620154a773
joaopedromartins <joao.pedro.brandao.martins@gmail.com>
2015-05-26 11:16:18 +0100

    signup
    adicionado xhtml template e signup

---
+++ p4-playlist-web/src/main/java/pt/uc/dei/aor/paj/Usersinterface.java
@@ -0,0 +1,87 @@
+package pt.uc.dei.aor.paj;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+
+@Named 
+@SessionScoped
+@RequestScoped
+public class Usersinterface implements Serializable {
+	private static final long serialVersionUID = -8310185641498834904L;
+
+	private String username;
+	private String password;
+	private String cpassword;
+	private String msgerro;
+	private String userLogged;
+	private HttpSession session;
+
+
+	public Usersinterface() {
+		this.setUserLogged(null);
+		this.session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+	}
+
+	//Getter e Setter associados à variável username
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	//Getter e Setter associados à variável password
+	//public String getPassword() {
+	//	return password;
+	//}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	//Getter e Setter associados à variável cpassword
+	//public String getCpassword() {
+	//	return cpassword;
+	//}
+	public void setCpassword(String cpassword) {
+		this.cpassword = cpassword;
+	}
+
+	//Getter associados à variável userlogged
+	public String getUserLogged() {
+		//System.out.println("get user logges="+userLogged);
+		return userLogged;
+	}
+	public void setUserLogged(String userLogged) {
+		this.userLogged = userLogged;
+		//System.out.println("set user logges="+userLogged);
+	}
+
+	//Getter associados à variável msgerro
+	public String getMsgerro() {
+		return msgerro;
+	}
+	public void setMsgerro(String msgerro) {
+		this.msgerro = msgerro;
+	}
+
+	//funcao para efectuar logout
+	public String userlogout() {
+		this.setUsername(null);
+		this.setPassword(null);
+		this.setUserLogged(null);
+		session.setAttribute("loggedin", false);
+		return "/resources/paginas/login";
+	}
+
+	
+	//funcao para efectuar registo de utilizador
+	public String usersignup() {
+		
+	}
+}
+
