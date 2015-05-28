9ebcba1ecee9a1aab918463a66520a51376a7e14
joaopedromartins <joao.pedro.brandao.martins@gmail.com>
2015-05-28 17:58:07 +0100

    add playlist
    add playlist ok.
    remove playlist error.

--- p4-playlist-ejb/src/main/java/pt/uc/dei/aor/paj/PlaylistEJB.java
+++ p4-playlist-ejb/src/main/java/pt/uc/dei/aor/paj/PlaylistEJB.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 @Stateless
 @LocalBean
@@ -27,19 +28,68 @@ public class PlaylistEJB {
 	public boolean addPlaylist(String username, String playlistname) {
     	
     	if (username.length() <= 2) return false;
-    	if (playlistname.length() <= 2) return false;
-    	User loggedUser = loginEJB.findUserByUsername(username);
     	
-    	System.out.println("EJB Logged user: "+loggedUser);
+    	if (playlistname.length() <= 2) return false;
+    	
+    	User loggedUser = loginEJB.findUserByUsername(username);
+    	//System.out.println("EJB Logged user: "+loggedUser);
     	if ( loggedUser == null) return false;
+    	
+    	//testar se ja exite playlist com esse nome
+    	//TypedQuery<Playlist> q = em.createQuery("from Playlist l where l.user = :user and l.title like :title", Playlist.class);
+		//q.setParameter("user", loggedUser);
+		//q.setParameter("title", playlistname);
+		//Playlist l = q.getResultList().get(0);
+		//if ( l != null) return false;
+    	
+    	//adiciona playlist
     	Playlist newPlaylist = new Playlist(loggedUser , playlistname);
-    	System.out.println("EJB New Playlist: "+newPlaylist);
     	em.persist( newPlaylist );
     	
     	return true;
     }
 	
+public boolean delPlaylist(String username, String playlistname) {
+    	
+    	if (username.length() <= 2) return false;
+    	if (playlistname.length() <= 2) return false;
+    	
+    	//testar se exite o utilizador com esse nome
+    	User loggedUser = loginEJB.findUserByUsername(username);
+    	System.out.println("EJB del Logged user: "+loggedUser);
+    	if ( loggedUser == null) return false;
+    	
+    	//testar se exite playlist com esse nome
+		//TypedQuery<Playlist> q = em.createQuery("from Playlist l where l.user = :user and l.title like :title", Playlist.class);
+		//q.setParameter("user", loggedUser);
+		//q.setParameter("title", playlistname);
+		//Playlist l = q.getResultList().get(0);
+		//if ( l == null) return false;
+    	
+		//apagar playlist
+    	em.createQuery("delete from Playlist l where l.name like :username and l.title like :title").
+		setParameter("username", loggedUser).
+    	setParameter("title", playlistname).executeUpdate();
+    	
+    	return true;
+    }
 	
+	
+	public List<String> listPlaylist(String username) {
+    	
+    	if (username.length() <= 2) return null;
+    	User loggedUser = loginEJB.findUserByUsername(username);
+    	
+    	//System.out.println("EJB list Logged user: "+loggedUser);
+    	if ( loggedUser == null) {
+    		return null;
+    	}
+    	
+    	TypedQuery<String> q = em.createQuery("select title from Playlist l where l.user = :user", String.class);
+		q.setParameter("user", loggedUser);
+		List<String> l = q.getResultList();
 		
+    	return l;
+    }	
 
 }
--- p4-playlist-web/src/main/java/pt/uc/dei/aor/paj/Playlistinterface.java
+++ p4-playlist-web/src/main/java/pt/uc/dei/aor/paj/Playlistinterface.java
@@ -1,9 +1,11 @@
 package pt.uc.dei.aor.paj;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpSession;
@@ -16,8 +18,8 @@ public class Playlistinterface implements Serializable {
 	private String msgerro;
 	private String username;
 	private String playlistname;
-	private String playlistID;
 	private String operacao;
+	private List<String> listaplaylistnames;
 	
 	@Inject
 	private Usersinterface loggeduser;
@@ -30,23 +32,7 @@ public class Playlistinterface implements Serializable {
 	}
 
 	
-	//Getter associados à variável userloggedID
-	public String getUserLogged() {
-		return username;
-	}
-	public void setUserLogged(String userLoggedID) {
-		this.username = userLoggedID;
-	}
-
-	//Getter associados à variável playlistID
-	public String getPlaylistID() {
-		return playlistID;
-	}
-	public void setPlaylistID(String playlistID) {
-		this.playlistID = playlistID;
-	}
-
-	//Getter associados à variável playlistname
+	//Getter associados à variável operacao
 	public String getOperacao() {
 		return operacao;
 	}
@@ -70,6 +56,16 @@ public class Playlistinterface implements Serializable {
 	public void setMsgerro(String msgerro) {
 		this.msgerro = msgerro;
 	}
+
+	//Getter associados à variável msgerro
+	public List<String> getListaplaylistnames() {
+		this.listaplaylistnames=playlist.listPlaylist(loggeduser.getUsername());
+		return listaplaylistnames;
+	}
+	public void setListaplaylistnames(List<String> listaplaylistnames) {
+		this.listaplaylistnames = listaplaylistnames;
+	}
+
 
 	//metodo que identifica se radiobotton esta activo na posicao criar 
 	public boolean typecriar() {
@@ -100,10 +96,37 @@ public class Playlistinterface implements Serializable {
 
 	//metodo para criar uma playlist
 	public String criaplaylist() {
-		System.out.println("Adicionar playlist:" + this.playlistname + "\t do user: "+this.username);
+		//System.out.println("Adicionar playlist:" + this.playlistname + "\t do user: "+this.username);
 		playlist.addPlaylist( loggeduser.getUserLogged(), playlistname);
 		
-		return "playlist";
+		return "/resources/secure/playlist?faces-redirect=true";
+	}
+	
+	//metodo para editar uma playlist
+	public String editaplaylist() {
+		System.out.println("Editar playlist:" + this.playlistname + "\t do user: "+this.username);
+		//playlist.editPlaylist( loggeduser.getUserLogged(), playlistname);
+		
+		return "/resources/secure/playlist?faces-redirect=true";
+	}
+	
+	//metodo para apagar uma playlist
+	public String apagaplaylist() {
+		System.out.println("Apagar playlist:" + this.playlistname + "\t do user: "+this.username);
+		playlist.delPlaylist( loggeduser.getUserLogged(), playlistname);
+		
+		
+		return "/resources/secure/playlist?faces-redirect=true";
 	}
 
+	
+	//metodo para seleccionar uma playlist
+	public String selectplaylist(ActionEvent ae) {
+		//atribui o nome da playlist correspondente ao botao linha seleccionada
+		this.playlistname = (String)ae.getComponent().getAttributes().get("line");
+		
+		return "/resources/secure/playlist?faces-redirect=true";
+	}
+	
+	
 }
